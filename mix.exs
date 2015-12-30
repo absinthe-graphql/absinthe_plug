c04a54c1c2ee64b2399e2a34659bb6c66d8d8e84
Ben Wilson <benwilson512@gmail.com>
2015-12-29 21:50:25 -0500

    hex publication changes

--- mix.exs
+++ mix.exs
@@ -7,7 +7,17 @@ defmodule AbsinthePlug.Mixfile do
      elixir: "~> 1.2-dev",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
+     package: package,
+     docs: [extras: ["README.md", "CONTRIBUTING.md"], main: "README"],
      deps: deps]
+  end
+
+  defp package do
+    [description: "A plug for Absinthe, a GraphQL implementation in Elixir",
+     files: ["lib", "mix.exs", "README*"],
+     maintainers: ["Ben Wilson", "Bruce Williams"],
+     licenses: ["Apache2"],
+     links: %{github: "https://github.com/CargoSense/absinthe_plug"}]
   end
 
   def application do
@@ -18,7 +28,9 @@ defmodule AbsinthePlug.Mixfile do
     [
       {:plug, "~> 1.0"},
       {:absinthe, git: "git@github.com:CargoSense/absinthe.git"},
-      {:poison, ">= 0.0.0", only: [:dev, :test]}
+      {:poison, ">= 0.0.0", only: [:dev, :test]},
+      {:ex_doc, "~> 0.11.0", only: :dev},
+      {:earmark, "~> 0.1.19", only: :dev},
     ]
   end
 end
