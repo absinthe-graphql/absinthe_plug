b3037bf96cbc68d639cffc7bd6b5a38470de264f
James Fish <james@fishcakez.com>
2017-02-10 14:56:35 +0000

    Update to Absinthe 1.2.6

--- mix.exs
+++ mix.exs
@@ -34,7 +34,7 @@ defmodule Absinthe.Plug.Mixfile do
   defp deps do
     [
       {:plug, "~> 1.2"},
-      {:absinthe, "~> 1.2.5", github: "absinthe-graphql/absinthe", branch: "master"},
+      {:absinthe, "~> 1.2.6"},
       {:poison, ">= 0.0.0", only: [:dev, :test]},
       {:ex_doc, "~> 0.14.0", only: :dev},
       {:earmark, "~> 1.1.0", only: :dev},
