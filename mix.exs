d9af9dbbd0040c1b5f7b960450b8b6f6a3ed531a
Ben Wilson <benwilson512@gmail.com>
2020-05-14 15:09:16 -0400

    update ex_doc

--- mix.exs
+++ mix.exs
@@ -45,7 +45,7 @@ defmodule Absinthe.Plug.Mixfile do
       {:plug, "~> 1.3.2 or ~> 1.4"},
       {:absinthe, "~> 1.5.0"},
       {:jason, ">= 0.0.0", only: [:dev, :test]},
-      {:ex_doc, "~> 0.20.2", only: :dev}
+      {:ex_doc, "~> 0.20", only: :dev}
     ]
   end
 end
